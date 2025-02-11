0f46296f33fcdc7ab34328c56b6408202aad0fb4
Radek <radek.musil@lematec.cz>
2025-02-11 17:43:07 +0100

    adding graph plotting

--- text.py
+++ text.py
@@ -1,9 +1,13 @@
 import pandas as pd # type: ignore
 import re
+import matplotlib.pyplot as plt
 
 
 text_file = "./DATA_BLOCK dbd_testbuffer.txt"
 data_dict = {}
+thickness = 0.76
+upper_trshld = round(1.3 * thickness, 3)
+lower_trshld = round(0.7 * thickness, 3)
 
 pattern = re.compile(
     r'bufferData\[(\d+)\]\.(blankFromCell|'
@@ -44,4 +48,35 @@ filtered_dict = {
 }
 
 df_new = pd.DataFrame.from_dict(filtered_dict, orient='index')
+
 df_new.to_excel('output.xlsx', index=False)
+
+df_new = df_new[
+    (df_new['dbdTrspMeasurement[1]'] != 0) &
+    (df_new['dbdTrspMeasurement[2]'] != 0) & 
+    (df_new['dbdTrspMeasurement[3]'] != 0) & 
+    (df_new['dbdTrspMeasurement[4]'] != 0)
+]
+
+
+fig, axes = plt.subplots(2, 1, figsize=(28, 10))
+df_new.plot(x='bufferData', y=['dbdTrspMeasurement[1]', 'dbdTrspMeasurement[2]'], kind='line', ax=axes[0])
+axes[0].axhline(y=upper_trshld, color='yellow', linestyle='--', label=f'Threshold ({upper_trshld})')
+axes[0].axhline(y=lower_trshld, color='yellow', linestyle='--', label=f'Threshold ({lower_trshld})')
+axes[0].set_title("DBD controller 1 measurements tracking")
+axes[0].set_xlabel("")
+axes[0].set_ylabel("")
+axes[0].legend()
+axes[0].grid(True)
+
+df_new.plot(x='bufferData', y=['dbdTrspMeasurement[3]', 'dbdTrspMeasurement[4]'], kind='line', ax=axes[1])
+axes[1].axhline(y=upper_trshld, color='yellow', linestyle='--', label=f'Threshold ({upper_trshld})')
+axes[1].axhline(y=lower_trshld, color='yellow', linestyle='--', label=f'Threshold ({lower_trshld})')
+axes[1].set_title("DBD controller 2 measurements tracking")
+axes[1].set_xlabel("")
+axes[1].set_ylabel("")
+axes[1].legend()
+axes[1].grid(True)
+
+plt.tight_layout()
+plt.show()
