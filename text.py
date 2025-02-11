b9ad27b7c53e1cd00ab342d0db816327face7787
Radek <radek.musil@lematec.cz>
2025-02-11 16:01:58 +0100

    working version of txt processing

--- text.py
+++ text.py
@@ -1,33 +1,47 @@
 import pandas as pd # type: ignore
+import re
 
 
-text_file = './LDATA_BLOCK dbd_testbuffer.txt'
+text_file = "./DATA_BLOCK dbd_testbuffer.txt"
+data_dict = {}
+
+pattern = re.compile(
+    r'bufferData\[(\d+)\]\.(blankFromCell|'
+    r'dbdGtyMeasurement\[(1|3)\]|'
+    r'visionCorrectionData\[1\]\.(X|Y|Rotation|Quality)|'
+    r'dbdTrspMeasurement\[(1|2|3|4)\]) := ([\d\.-]+);'
+)
+
 with open(text_file, "r") as file:
      for line in file:
-        print(line.strip())
+        match = pattern.search(line)
+        if match:
+            buffer_index = int(match.group(1))
+            key = match.group(2)
+            value = float(match.group(6))
 
+            if buffer_index not in data_dict:
+                data_dict[buffer_index] = {
+                    'bufferData': buffer_index,
+                    'blankFromCell': None,
+                    'dbdGtyMeasurement[1]': None,
+                    'dbdGtyMeasurement[3]': None,
+                    'X': None,
+                    'Y': None,
+                    'Rotation': None,
+                    'Quality': None,
+                    'dbdTrspMeasurement[1]': None,
+                    'dbdTrspMeasurement[2]': None,
+                    'dbdTrspMeasurement[3]': None,
+                    'dbdTrspMeasurement[4]': None,
+                }
+            if key.startswith("visionCorrectionData[1]."):
+                key = key.split(".")[-1]
+            data_dict[buffer_index][key] = value
 
-new_columns = [
-    'bufferData', 'blankFromCell', 'dbdGtyMeasurement[1]', 'dbdGtyMeasurement[3]',
-    'X', 'Y', 'Rotation', 'Quality', 'dbdTrspMeasurement[1]', 'dbdTrspMeasurement[2]', 'dbdTrspMeasurement[3]', 'dbdTrspMeasurement[4]'
-]
-df_new = pd.DataFrame(columns=new_columns)
+filtered_dict = {
+    k: v for k, v in data_dict.items() if v['blankFromCell'] is None or int(v['blankFromCell']) != 0
+}
 
-for i in range(1, 501):
-    new_record = {
-        'bufferData': 4,
-        'blankFromCell': 4,
-        'dbdGtyMeasurement[1]': 4,
-        'dbdGtyMeasurement[3]': 4,
-        'X': 4,
-        'Y': 4,
-        'Rotation': 4,
-        'Quality': 4,
-        'dbdTrspMeasurement[1]': 4,
-        'dbdTrspMeasurement[2]': 4,
-        'dbdTrspMeasurement[3]': 4,
-        'dbdTrspMeasurement[4]': 4
-    }
-    df_new.loc[len(df_new)] = new_record
-
+df_new = pd.DataFrame.from_dict(filtered_dict, orient='index')
 df_new.to_excel('output.xlsx', index=False)
