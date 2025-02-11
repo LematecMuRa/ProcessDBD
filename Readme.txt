f1f5d43f68ac4a2ba89ca31a62022dfe639e71ff
Radek <radek.musil@lematec.cz>
2025-02-11 19:58:29 +0100

    bugfixing plot, opt to plot stack change

--- Readme.txt
+++ Readme.txt
@@ -3,3 +3,4 @@
 3. insert data in the same folder as program in .txt format named: DATA_BLOCK dbd_testbuffer
 4. run: python txt.py
 5. you can add parametr of the blank thickness (default 0.76) -> run python txt.py 0.7
+6. you can add second parametr to display stack change in the plot -> run python txt.py 0.7 true
--- text.py
+++ text.py
@@ -6,11 +6,17 @@ import sys
 
 text_file = "./DATA_BLOCK dbd_testbuffer.txt"
 data_dict = {}
+row_indices_stac_change = []
 
 if len(sys.argv) > 1:
     thickness = float(sys.argv[1])
 else:
     thickness = 0.76
+
+if len(sys.argv) > 2:
+    disp_stack_change = sys.argv[2].lower() in ['true', '1', 't', 'y', 'yes']
+else:
+    disp_stack_change = False
 
 upper_trshld = round(1.3 * thickness, 3)
 lower_trshld = round(0.7 * thickness, 3)
@@ -57,16 +63,17 @@ df_new = pd.DataFrame.from_dict(filtered_dict, orient='index')
 
 df_new.to_excel('output.xlsx', index=False)
 
-df_new = df_new[
-    (df_new['dbdTrspMeasurement[1]'] != 0) &
-    (df_new['dbdTrspMeasurement[2]'] != 0) & 
-    (df_new['dbdTrspMeasurement[3]'] != 0) & 
-    (df_new['dbdTrspMeasurement[4]'] != 0)
-]
+for index, row in df_new.iterrows():
+    if (row['dbdTrspMeasurement[1]'] == 0 and 
+        row['dbdTrspMeasurement[2]'] == 0 and 
+        row['dbdTrspMeasurement[3]'] == 0 and 
+        row['dbdTrspMeasurement[4]'] == 0):
+            row_indices_stac_change.append(index)
 
+df_dropped = df_new.drop(row_indices_stac_change)
 
 fig, axes = plt.subplots(2, 1, figsize=(28, 10))
-df_new.plot(x='bufferData', y=['dbdTrspMeasurement[1]', 'dbdTrspMeasurement[2]'], kind='line', ax=axes[0])
+df_dropped.plot(x='bufferData', y=['dbdTrspMeasurement[1]', 'dbdTrspMeasurement[2]'], kind='line', ax=axes[0])
 axes[0].axhline(y=upper_trshld, color='yellow', linestyle='--', label=f'Threshold ({upper_trshld})')
 axes[0].axhline(y=lower_trshld, color='yellow', linestyle='--', label=f'Threshold ({lower_trshld})')
 axes[0].set_title("DBD controller 1 measurements tracking")
@@ -75,7 +82,7 @@ axes[0].set_ylabel("")
 axes[0].legend()
 axes[0].grid(True)
 
-df_new.plot(x='bufferData', y=['dbdTrspMeasurement[3]', 'dbdTrspMeasurement[4]'], kind='line', ax=axes[1])
+df_dropped.plot(x='bufferData', y=['dbdTrspMeasurement[3]', 'dbdTrspMeasurement[4]'], kind='line', ax=axes[1])
 axes[1].axhline(y=upper_trshld, color='yellow', linestyle='--', label=f'Threshold ({upper_trshld})')
 axes[1].axhline(y=lower_trshld, color='yellow', linestyle='--', label=f'Threshold ({lower_trshld})')
 axes[1].set_title("DBD controller 2 measurements tracking")
@@ -83,6 +90,11 @@ axes[1].set_xlabel("")
 axes[1].set_ylabel("")
 axes[1].legend()
 axes[1].grid(True)
+
+if disp_stack_change:
+    for idx in row_indices_stac_change:
+        axes[0].axvline(x=df_dropped['bufferData'].iloc[idx], color='green', linestyle='--', linewidth=0.5)
+        axes[1].axvline(x=df_dropped['bufferData'].iloc[idx], color='green', linestyle='--', linewidth=0.5)
 
 plt.tight_layout()
 plt.show()
